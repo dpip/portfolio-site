f526732de5101613e836a2b1319658b1d198848c
Daniel <dpip.nc@gmail.com>
2020-09-09 18:14:15 -0400

    creats credits section

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,6 @@
 import React from "react"
 import "../assets/scss/footer.scss"
+import Avatar from "../assets/images/dpip.jpeg"
 import { Link } from "gatsby"
 import TransitionLink from "gatsby-plugin-transition-link"
 import AniLink from "gatsby-plugin-transition-link/AniLink"
@@ -9,7 +10,9 @@ export default (props) =>
 <footer>
   <div className={'container'}>
     <div className={'footer__top'}>
-      <AniLink className={'avatar avatar__small'} paintDrip hex={'#e6e6e6'} to="/">Avatar</AniLink>
+      <AniLink className={'avatar avatar__small'} paintDrip hex={'#e6e6e6'} to="/">
+        <img width={'35'} height={'35'} src={Avatar}/>
+      </AniLink>
     </div>
     <div className={'row footer__bottom'}>
       <a className={'social-item'}>Linkedin</a>
@@ -17,5 +20,6 @@ export default (props) =>
       <a className={'social-item'}>Codepen</a>
       <a className={'social-item'}>Contact</a>
     </div>
+    <p className={'credits'}>👾Built with&nbsp;<a href={'https://www.gatsbyjs.com/'} target={'blank'}>Gatsby</a> and&nbsp;<a href={'https://www.npmjs.com/package/react-particles-js'} target={'blank'}>react-particles</a>👾</p>
   </div>
 </footer>
