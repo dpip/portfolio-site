033c09bca3af4ae0350305e2830ffd1e3211d0e2
dpip <dpip.nc@gmail.com>
2020-10-26 11:32:52 -0400

    moves card component into work component // migrates card json to content json

--- src/components/sections/work.js
+++ src/components/sections/work.js
@@ -1,12 +1,29 @@
 import React from "react"
 import "../../assets/scss/work.scss"
 
+import Card from "../../components/card.js"
+
 export default props => (
   <>
     <h2 id={"work"} className={"heading-section"} style={{ marginTop: "2rem" }}>
       <span>{props.content.bodyTitle}</span>
       <div className="rule"></div>
     </h2>
-    <section className={"container work-wrapper"}></section>
+    <section className={"container work-wrapper"}>
+      <div className={"card--grid"}>
+        {props.content.cards.map((card, index) => (
+          <div className="card--wrapper" key={index}>
+            <Card
+              key={index}
+              source={card.source}
+              title={card.title}
+              description={card.description}
+              cta={card.cta}
+              link={card.link}
+            />
+          </div>
+        ))}
+      </div>
+    </section>
   </>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,6 @@ import content from "../data/content.json"
 
 import "../assets/scss/main.scss"
 import Layout from "../components/layout.js"
-import Card from "../components/card.js"
 import Intro from "../components/sections/intro.js"
 import About from "../components/sections/about.js"
 import Work from "../components/sections/work.js"
@@ -20,21 +19,6 @@ export default function Home() {
           <About content={content.about} />
           <Work content={content.work} />
           <Experimental content={content.experiments} />
-          <h2 style={{ marginTop: "2rem" }}>Professional Work</h2>
-          <div className={"card--grid"}>
-            {cardData.map((card, index) => (
-              <div className="card--wrapper" key={index}>
-                <Card
-                  key={index}
-                  source={card.source}
-                  title={card.title}
-                  description={card.description}
-                  cta={card.cta}
-                  link={card.link}
-                />
-              </div>
-            ))}
-          </div>
         </div>
       </section>
     </Layout>
