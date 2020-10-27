8fbf2231f9bc9ce0605391afaa8d4c89e387bc6a
dpip <dpip.nc@gmail.com>
2020-10-27 14:45:01 -0400

    form styling // props determine success or error types for modal component // error modal now has function passed in to close modal seperate from anilink // content json entries for modal content

--- src/components/form.js
+++ src/components/form.js
@@ -1,6 +1,9 @@
 import React from "react"
 import "../assets/scss/contact.scss"
 import Modal from "../components/modal.js"
+import content from "../data/content.json"
+
+import Fade from "react-reveal/Fade"
 
 export default class Form extends React.Component {
   constructor(props) {
@@ -8,51 +11,86 @@ export default class Form extends React.Component {
     this.submitForm = this.submitForm.bind(this)
     this.state = {
       status: "",
+      open: false,
     }
   }
 
   render() {
     const { status } = this.state
+    const sm = content.form.success
+    const em = content.form.error
+
+    const resetModal = () => {
+      this.setState({
+        status: "",
+        open: false,
+      })
+    }
+
     return (
-      <form
-        onSubmit={this.submitForm}
-        action="https://formspree.io/meqryvqd"
-        method="POST"
-      >
-        <br />
-        <div className="form__name-email">
-          <div className="name-email-item">
-            <label>Your Email</label>
-            <input
-              id={"email"}
-              type="email"
-              name="email"
-              placeholder="Your email..."
-            />
+      <Fade bottom cascade duration={500} distance={"24px"}>
+        <form
+          onSubmit={this.submitForm}
+          action="https://formspree.io/meqryvqd"
+          method="POST"
+        >
+          <br />
+          <div className="form__name-email">
+            <div className="name-email-item">
+              <label>Your Email</label>
+              <input
+                id={"email"}
+                type="email"
+                name="email"
+                placeholder="Your email..."
+              />
+            </div>
+            <div className="name-email-item">
+              <label>Your Name</label>
+              <input
+                id={"name"}
+                type="name"
+                name="name"
+                placeholder="Your name..."
+              />
+            </div>
           </div>
-          <div className="name-email-item">
-            <label>Your Name</label>
-            <input
-              id={"name"}
-              type="name"
-              name="name"
-              placeholder="Your name..."
+          <br />
+          <label>Message</label>
+          <textarea
+            id={"message"}
+            type="text"
+            name="message"
+            placeholder="Hey Pip..."
+            cols="30"
+            rows="10"
+          />
+          {status === "SUCCESS" ? (
+            <Modal
+              type={"success"}
+              header={sm.header}
+              message={sm.message}
+              btnText={"back home"}
+              to={sm.to}
+              open={this.state.open}
+              resetModal={resetModal}
             />
-          </div>
-        </div>
-        <br />
-        <label>Message</label>
-        <textarea
-          id={"message"}
-          type="text"
-          name="message"
-          placeholder="Hey Daniel..."
-          cols="30"
-          rows="10"
-        />
-        {status === "SUCCESS" ? <Modal /> : <button>Submit</button>}
-        {status === "ERROR" && <p>Ooops! There was an error.</p>}
-      </form>
+          ) : (
+            <button>Submit</button>
+          )}
+          {status === "ERROR" && (
+            <Modal
+              type={"error"}
+              header={em.header}
+              message={em.message}
+              btnText={"gotcha"}
+              to={em.to}
+              open={this.state.open}
+              resetModal={resetModal}
+            />
+          )}
+        </form>
+      </Fade>
     )
   }
 
@@ -67,9 +105,9 @@ export default class Form extends React.Component {
       if (xhr.readyState !== XMLHttpRequest.DONE) return
       if (xhr.status === 200) {
         form.reset()
-        this.setState({ status: "SUCCESS" })
+        this.setState({ status: "SUCCESS", open: true })
       } else {
-        this.setState({ status: "ERROR" })
+        this.setState({ status: "ERROR", open: true })
       }
     }
     xhr.send(data)
--- src/components/modal.js
+++ src/components/modal.js
@@ -1,23 +1,71 @@
-import React from 'react'
-import TransitionLink from "gatsby-plugin-transition-link"
+import React, { useState } from "react"
 import AniLink from "gatsby-plugin-transition-link/AniLink"
-import '../assets/scss/modal.scss'
+import "../assets/scss/modal.scss"
 
-import Button from "../components/button.js";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  faCodepen,
+  faLinkedin,
+  faGithub,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons"
 
-const Modal = () => (
-  <div id={'modal'}>
-    <div className={'modal__content'}>
-      <h1>Thanks for reaching out!</h1>
-      <p>If you'd like to see what else I'm up to feel free to check out social links below or feel free to return home and check out some more projects I have worked on.</p>
-      <div className={'modal__social'}>
-        <a className={'social-item'} style={{paddingLeft: '0px'}}>Linkedin</a>
-        <a className={'social-item'}>Github</a>
-        <a className={'social-item'}>Codepen</a>
-      </div>
-      <AniLink className="btn" paintDrip hex={'#e6e6e6'} to="/">Return home</AniLink>
-    </div>
-  </div>
-)
+import Fade from "react-reveal/Fade"
+
+const Modal = props => {
+  const [modal, modalOpen] = useState(props.open)
+
+  const closeModal = e => {
+    e.preventDefault()
+    props.resetModal()
+    return
+  }
+
+  return (
+    <>
+      {modal === true ? (
+        <div id={"modal"}>
+          <Fade bottom cascade duration={500} distance={"24px"}>
+            <div className={"modal__content"}>
+              <h1>{props.header}</h1>
+              <p className={"modal__content-message"}>{props.message}</p>
+              <div className={"modal__social"}>
+                <a className={"social-item"} style={{ paddingLeft: "0px" }}>
+                  <FontAwesomeIcon icon={faCodepen} />
+                </a>
+                <a className={"social-item"}>
+                  <FontAwesomeIcon icon={faGithub} />
+                </a>
+                <a className={"social-item"}>
+                  <FontAwesomeIcon icon={faLinkedin} />
+                </a>
+                <a className={"social-item"}>
+                  <FontAwesomeIcon icon={faInstagram} />
+                </a>
+                <div className={"rule-custom"} />
+              </div>
+              {props.type === "success" ? (
+                <AniLink
+                  className="btn"
+                  paintDrip
+                  hex={"#e6e6e6"}
+                  to={props.to}
+                >
+                  {props.btnText}
+                </AniLink>
+              ) : (
+                <div className={"btn green"} onClick={e => closeModal(e)}>
+                  {props.btnText}
+                </div>
+              )}
+            </div>
+          </Fade>
+        </div>
+      ) : (
+        <></>
+      )}
+    </>
+  )
+}
 
 export default Modal
