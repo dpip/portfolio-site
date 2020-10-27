3c20e865915479f30838392400d0b5181289f577
dpip <dpip.nc@gmail.com>
2020-10-27 11:48:26 -0400

    mobile styles for form inputs // button styles for form submission

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,17 +1,20 @@
 import React from "react"
 import "../assets/scss/main.scss"
 import "../assets/scss/contact.scss"
-import Layout from "../components/layout.js";
-import Form from "../components/form.js";
+import Layout from "../components/layout.js"
+import Form from "../components/form.js"
 
-export default (props) =>
-
+export default props => (
   <Layout>
     <section>
-      <div className={'container'}>
-        <h1 className={'red'}>Let's get in touch!</h1>
-        <p><strong>Interested in working together?</strong> Fill out the form below and we can get started on creating something amazing together!</p>
+      <div className={"container container--form"}>
+        <h1 className={"green"}>Let's get in touch!</h1>
+        {/* <p>
+          <span>Interested in working together?</span> Fill out the form below
+          and we can get started on creating something amazing together!
+        </p> */}
         <Form />
       </div>
     </section>
   </Layout>
+)
