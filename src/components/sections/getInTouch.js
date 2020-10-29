684f9365533e8eb2877a8f5d0d33a7504c75b742
dpip <dpip.nc@gmail.com>
2020-10-29 18:26:07 -0400

    disables scrolling for mobile devices for modal

--- src/components/sections/getInTouch.js
+++ src/components/sections/getInTouch.js
@@ -6,26 +6,24 @@ import AniLink from "gatsby-plugin-transition-link/AniLink"
 import Fade from "react-reveal/Fade"
 
 export default props => (
-  <Fade bottom cascade distance={"20px"}>
-    <div id={"contact"} className={"container container--get-in-touch"}>
-      <h2 className={"heading-section"} style={{ marginTop: "2rem" }}>
-        <span className={"heading-section--number"}>04.</span>
-        <span>Get in touch</span>
-        <div className="rule"></div>
-      </h2>
-      <p>
-        I'm currently looking for new opportunities and my inbox is always open!
-        Whether you have a question or just want to say hi, I'll try my best to
-        get back to you!
-      </p>
-      <AniLink
-        className="btn btn-contact"
-        paintDrip
-        hex={"#e6e6e6"}
-        to="/contact"
-      >
-        Contact
-      </AniLink>
-    </div>
-  </Fade>
+  <div id={"contact"} className={"container container--get-in-touch"}>
+    <h2 className={"heading-section"} style={{ marginTop: "2rem" }}>
+      <span className={"heading-section--number"}>04.</span>
+      <span>Get in touch</span>
+      <div className="rule"></div>
+    </h2>
+    <p>
+      I'm currently looking for new opportunities and my inbox is always open!
+      Whether you have a question or just want to say hi, I'll try my best to
+      get back to you!
+    </p>
+    <AniLink
+      className="btn btn-contact"
+      paintDrip
+      hex={"#e6e6e6"}
+      to="/contact"
+    >
+      Contact
+    </AniLink>
+  </div>
 )
