7063b17fdc76a94a8dabcacb07d83b657125abb4
dpip <dpip.nc@gmail.com>
2020-10-29 17:30:47 -0400

    edits load screen slightly for keyframe timing // form js has a scrolltop listener // contact id moved up to parent container

--- src/components/form.js
+++ src/components/form.js
@@ -1,9 +1,10 @@
 import React from "react"
+
+import scrollTo from "gatsby-plugin-smoothscroll"
+
 import "../assets/scss/contact.scss"
 import Modal from "../components/modal.js"
 import content from "../data/content.json"
-
-import Fade from "react-reveal/Fade"
 
 export default class Form extends React.Component {
   constructor(props) {
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,16 +1,25 @@
 import React from "react"
+
+import scrollTo from "gatsby-plugin-smoothscroll"
+
 import "../assets/scss/main.scss"
 import "../assets/scss/contact.scss"
 import Layout from "../components/layout.js"
 import Form from "../components/form.js"
 
-export default props => (
-  <Layout type={"contact"}>
-    <section>
-      <div className={"container container--form"}>
-        <h1 className={"hello green"}>Let's connect!</h1>
-        <Form {...props} />
-      </div>
-    </section>
-  </Layout>
-)
+const Contact = props => {
+  setTimeout(() => {
+    scrollTo("#contact-form")
+  }, 250)
+  return (
+    <Layout type={"contact"}>
+      <section id="contact-form">
+        <div className={"container container--form"}>
+          <h1 className={"hello green"}>Let's connect!</h1>
+          <Form {...props} />
+        </div>
+      </section>
+    </Layout>
+  )
+}
+export default Contact
